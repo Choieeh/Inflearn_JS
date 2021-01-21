4346f79354d5c0aa3f6c2123ec394cdff3620df6
Choieeh <chyh0182@naver.com>
2021-01-21 14:19:01 +0000

    number baseball

--- numbaseball/num.js
+++ numbaseball/num.js
@@ -4,11 +4,10 @@ var 숫자후보 = [1,2,3,4,5,6,7,8,9];
 
 var 숫자배열 = [];
 
-for (var i = 0; i<4; i++){
-	var temp = 숫자후보.splice(Math.ceil(Math.random() * 숫자후보.length()), 1)[0];
+for (var i = 0; i<4; i+=1){
+	var temp = 숫자후보.splice(Math.floor(Math.random() * (9-i)), 1)[0];
 	숫자배열.push(temp);
 }
-
 
 var result = document.createElement('h1');
 document.body.append(result);
@@ -21,20 +20,24 @@ input.maxLength = 4;
 var button = document.createElement('button');
 button.textContent = '입력';
 form.append(button);
+console.log(숫자배열)
 
 
 form.addEventListener('submit', function callback(event) {
+	console.log(숫자배열)
+//왜 숫자배열이 이 함수안에서는 적용이 안되지???????
 	event.preventDefault();
-	var 답 = 입력창.value;
-	console.log(답);
-	if (답 === 숫자배열.join('')) {
+	var 답 = input.value;
+	console.log(답)
+	console.log(숫자배열.join(''))
+	if (답 === 숫자배열.join(``)) {
 		result.textContent = '홈런';
 		input.value = '';
 		input.focus();
 		var 숫자후보 = [1,2,3,4,5,6,7,8,9];
 		var 숫자배열 = [];
 		for (var i = 0; i<4; i++){
-			var temp = 숫자후보.splice(Math.ceil(Math.random() * 숫자후보.length()), 1)[0];
+			var temp = 숫자후보.splice(Math.floor(Math.random() * (9-i)), 1)[0];;
 			숫자배열.push(temp);
 		}
 	} else {
@@ -44,7 +47,12 @@ form.addEventListener('submit', function callback(event) {
 		for (var i =0; i < 3; i++){
 			if(답배열[i] === 숫자배열[i]){
 				strike++;
+			} else if (숫자배열.indexOf(답배열[i]) > -1){
+				ball++;
 			}
 		}
+		result.textContent = strike + '스트라이크' + ball + '볼 입니다.';
+		input.value = '';
+		input.focus();
 	}
-})
+});
