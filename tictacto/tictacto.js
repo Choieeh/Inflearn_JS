e351feaf90179c2479d66e09909fab284ca9ed69
Choieeh <chyh0182@naver.com>
2021-01-24 11:29:23 +0000

    tictacto making

--- tictacto/tictacto.js
+++ tictacto/tictacto.js
@@ -2,17 +2,40 @@ var body = document.body;
 var table = document.createElement('table');
 var count = 1;
 
-var board = new Array(9);
+var board = new Array(10);
 board.fill(0);
 var turn = 1;
-console.log(board);
+var end = 0;
 
-function if_win(){
-	for(var i =0; i<3;i++){
-		for(var j=0; j<3; j++){
-			
-		}
+function if_win(thiss, turn){
+	var turnm
+	var jul = thiss.id / 3;
+	var can = thiss.id % 3;
+	if(turn === 1){
+		turnm = 'X';
+	}else{
+		turnm = 'O';
 	}
+	if(
+		board[jul*3 + 1].textContent === turnm &&
+		board[jul*3 + 2].textContent === turnm &&
+		board[jul*3 + 3].textContent === turnm
+	) {
+		end = 1;
+	}
+	if(
+		board[can].textContent === turnm &&
+		board[can+3].textContent === turnm &&
+		board[can+6].textContent === turnm
+	){
+		end = 1;
+	}
+	if(jul - can === 1 || Math.abs(jul - can) === 3){
+		if(
+			
+		)
+	}
+	
 }
 
 var btn_click = function (){
@@ -20,10 +43,12 @@ var btn_click = function (){
 	if(board[this.id] === 0){
 		if(turn === 1){
 			board[this.id] = 'X';
+			this.textContent = 'X';
 			turn = 2;
 		}
 		else if (turn ===2){
 			board[this.id] = 'O';
+			this.textContent = 'O';
 			turn = 1;
 		}
 	}
